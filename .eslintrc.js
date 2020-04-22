8b6d9eec34446771ce256f6d0ca273fa1c9124f3
SeonHyungJo <seonhyung.jo@gmail.com>
2020-04-23 00:41:33 +0900

    📚 SeonHyungJo / Update eslintrc target version to 2019

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     graphql: false,
   },
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2019,
     sourceType: 'module',
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
